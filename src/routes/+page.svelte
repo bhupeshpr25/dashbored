4781e09f7c3af7400d4d9f6e3eeb93c9259e7348
Bhupesh <pradhanbhupesh0@gmail.com>
2024-04-19 21:55:39 +0530

    feat: add ui layout and assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,30 @@
-<h1>hello world</h1>
+<script lang="ts">
+	import * as Resizable from '$lib/components/ui/resizable/index';
+</script>
+
+<title>frontend exercise</title>
+
+<body class="h-screen max-w-full text-white">
+	<Resizable.PaneGroup direction="vertical" class="rounded-2xl">
+		<Resizable.Pane defaultSize={70}>
+			<Resizable.PaneGroup direction="horizontal" class="rounded-lg border">
+				<Resizable.Pane defaultSize={25} class="bg-gray-900">left section</Resizable.Pane>
+				<Resizable.Handle class="bg-gray-900" />
+				<Resizable.Pane defaultSize={50} class="bg-gray-900">middle section</Resizable.Pane>
+				<Resizable.Handle class="bg-gray-900" />
+				<Resizable.Pane defaultSize={25} class="bg-gray-900">
+					<div class="clock-background m-4 flex h-full items-center justify-center rounded-2xl">
+						right section
+					</div>
+				</Resizable.Pane>
+				<Resizable.Handle class="bg-gray-900" />
+			</Resizable.PaneGroup>
+		</Resizable.Pane>
+		<Resizable.Handle class="bg-gray-900" />
+		<Resizable.Pane defaultSize={30} class="bg-gray-900">
+			<div class="bg-morphic m-4 flex h-full items-center justify-center rounded-2xl">
+				bottom section
+			</div>
+		</Resizable.Pane>
+	</Resizable.PaneGroup>
+</body>
